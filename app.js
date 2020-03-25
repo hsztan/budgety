850a092bb13e78e84e22e599a4d49d762b746d46
github username <github email address>
2020-03-25 15:45:30 -0500

    adding event listeners into global app controller

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
+
+// BUDGET CONTROLLER
 var budgetController = (function () {
 
-  var x = 23;
 
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
 
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    console.log('Button was clicked!')
+  });
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13) {
+
     }
-  }
+    
+  });
 
 })(budgetController, UIController);
