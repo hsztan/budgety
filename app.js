8b08a985f1cae258bdc77382a318b27723b58772
github username <github email address>
2020-03-25 09:28:45 -0500

    testing modular structure and controllers

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var budgetController = (function () {
 
   return {
     publicTest: function(b) {
-      console.log(add(b));
+      return add(b);
     }
   }
 
@@ -16,4 +16,18 @@ var budgetController = (function () {
 
 var UIController = (function() {
 
+
+
 })();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z);
+    }
+  }
+
+})(budgetController, UIController);
