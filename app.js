6f33106c96c0c4e8e80a277e1a9a4cca929940b6
github username <github email address>
2020-03-27 08:48:54 -0500

    starts to create html element

--- app.js
+++ app.js
@@ -68,6 +68,15 @@ var UIController = (function() {
         value: document.querySelector(DOMstrings.valueType).value
       }
     },
+    addListItem: function() {
+      // Create HTML string with placeholder text
+
+      html = '<div class="item clearfix" id="income-0"> <div class="item__description">Salary</div> <div class="right clearfix"> <div class="item__value">+ 2,100.00</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+
+      // Replace the placeholder text with some actual data
+
+      // Insert the HTML into the DOM
+    },
     getDOMstrings: function() {
       return DOMstrings;
     }
