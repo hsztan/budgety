11920dd6b34df7abadfd79690c57c72db937d032
github username <github email address>
2020-03-28 19:11:35 -0500

    adds budget and percentage of expenses in data structure

--- app.js
+++ app.js
@@ -14,15 +14,25 @@ var budgetController = (function () {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(e) {
+      sum += e.value;
+    })
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
       inc: []
     },
     totals: {
-      exp:0,
+      exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
   
   return {
@@ -45,6 +55,15 @@ var budgetController = (function () {
       return newItem;
     },
     calculateBudget: function() {
+      // calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      // Calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // Calculate the percentage of income that we spent
+      data.percentage = (data.totals.exp / data.totals.inc) * 100;
 
     },
     testing: function() {
