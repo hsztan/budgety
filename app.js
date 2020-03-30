2ffe89ce69d54002b0f1aed59dd98e014a44d6cf
github username <github email address>
2020-03-30 17:37:44 -0500

    app basic structure and features working

--- app.js
+++ app.js
@@ -144,7 +144,11 @@ var UIController = (function() {
       document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
       document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
       document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
-      document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
     },
 
     getDOMstrings: function() {
@@ -203,6 +207,12 @@ var controller = (function(budgetCtrl, UICtrl) {
  return {
    init: function() {
       console.log('Application has started!');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
    }
  };
